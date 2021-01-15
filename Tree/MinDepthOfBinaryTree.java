676f1a7049125a37edd9d34ab69ba5de2bd0b122
kashish88 <65272887+kashish88@users.noreply.github.com>
2021-01-15 22:57:58 +0530

    Create MinDepthOfBinaryTree.java

---
+++ Tree/MinDepthOfBinaryTree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class MinDepthOfBinaryTree {
+    public int minDepth(TreeNode A) {
+        public int minDepth(TreeNode A) {
+            if(A==null)
+              return 0; 
+         
+           if(A.left==null && A.right==null)
+              return 1; 
+          
+           if(A.left==null)
+              return minDepth(A.right) +1; 
+              
+           if(A.right==null)
+               return minDepth(A.left) +1; 
+              
+           int lDepth= minDepth(A.left);
+           int rDepth= minDepth(A.right);
+           
+           return Math.min(lDepth,rDepth)+1; 
+       }
+    }
+}
