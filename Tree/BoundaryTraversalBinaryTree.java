e1520cb25091fa2bcd88e7e5927920ed3cdf0c20
kashish88 <65272887+kashish88@users.noreply.github.com>
2021-02-12 12:45:32 +0530

    Create BoundaryTraversalBinaryTree.java

---
+++ Tree/BoundaryTraversalBinaryTree.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+public class BoundaryTraversalBinaryTree{
+    public static void main(String args[]) 
+    { 
+        BinaryTree tree = new BinaryTree(); 
+        tree.root = new Node(20); 
+        tree.root.left = new Node(8); 
+        tree.root.left.left = new Node(4); 
+        tree.root.left.right = new Node(12); 
+        tree.root.left.right.left = new Node(10); 
+        tree.root.left.right.right = new Node(14); 
+        tree.root.right = new Node(22); 
+        tree.root.right.right = new Node(25); 
+        tree.printBoundary(tree.root); 
+    } 
+    Node root;
+    void printLeaves(Node node){
+        if(node==null)
+        return ;
+
+        printLeaves(node.left);
+        if(node.left==null && node.right==null)
+        System.out.println(node.data+" ");
+        printLeaves(node.right);
+    }
+    void printBoundaryLeft(Node node) {
+        if (node == null) 
+            return; 
+
+            if (node.left != null) { 
+               
+                System.out.print(node.data + " "); 
+                printBoundaryLeft(node.left); 
+            } 
+            else if (node.right != null) { 
+                System.out.print(node.data + " "); 
+                printBoundaryLeft(node.right); 
+            } 
+    }
+    void printBoundaryRight(Node node) {
+        if (node == null) 
+            return; 
+            if (node.right != null) { 
+                printBoundaryRight(node.right); 
+                System.out.print(node.data + " "); 
+            } 
+            else if (node.left != null) { 
+                printBoundaryRight(node.left); 
+                System.out.print(node.data + " "); 
+            } 
+    }
+    void printBoundary(Node node) 
+    { 
+        if (node == null) 
+            return; 
+  
+        System.out.print(node.data + " "); 
+  
+      
+        printBoundaryLeft(node.left); 
+  
+       
+        printLeaves(node.left); 
+        printLeaves(node.right); 
+  
+       
+        printBoundaryRight(node.right); 
+    } 
+}
+class Node { 
+    int data; 
+    Node left, right; 
+  
+    Node(int item) 
+    { 
+        data = item; 
+        left = right = null; 
+    } 
+} 
